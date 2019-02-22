c46cdd636ab848083366d350f6ad09529da99b35
csokaimola <csokaimola@gmail.com>
2019-02-22 16:54:56 +0100

    Create folders and make a script that says hello

---
+++ exam/csoka_imola/IT/my_script.sh
@@ -0,0 +1,3 @@
+mkdir csoka_imola/IT/ made_by_my_script
+echo "Hello world" > csoka_imola/IT/made_by_my_script/hello.txt
+ls csoka_imola/IT/made_by_my_script >  csoka_imola/IT/made_by_my_script/file_list.txt
