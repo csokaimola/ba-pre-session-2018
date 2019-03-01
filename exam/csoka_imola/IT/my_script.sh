bf93cb0b9d80c4207bd9ca98154b354dc9ecb602
csokaimola <csokaimola@gmail.com>
2019-03-01 10:06:20 +0100

    script valami

--- exam/csoka_imola/IT/my_script.sh
+++ exam/csoka_imola/IT/my_script.sh
@@ -1,3 +1,3 @@
-mkdir csoka_imola/IT/ made_by_my_script
-echo "Hello world" > csoka_imola/IT/made_by_my_script/hello.txt
-ls csoka_imola/IT/made_by_my_script >  csoka_imola/IT/made_by_my_script/file_list.txt
+mkdir exam/csoka_imola/IT/made_by_my_script
+echo "Hello world" > exam/csoka_imola/IT/made_by_my_script/hello.txt
+ls exam/csoka_imola/IT/made_by_my_script >  exam/csoka_imola/IT/made_by_my_script/file_list.txt
